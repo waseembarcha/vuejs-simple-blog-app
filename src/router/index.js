8fe0ca5d5f55b4d744f0f0fb55c207237b0a8065
waseembarcha <waseembarcha1993@gmail.com>
2018-04-11 15:50:46 +0500

    modified

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import AddBlog from '@/components/AddBlog'
 import ViewBlogs from '@/components/ViewBlogs'
 
